e9e6aee536a563d184bd3445300143b6a673955e
Olesia <85791382+caleyass@users.noreply.github.com>
2024-10-08 16:15:01 +0300

    init project woth permissions

---
+++ Lab4CameraPetrova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab4CameraPetrova
+//
+//  Created by Olesia Petrova on 08.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
